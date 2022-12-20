0d1b6d3cf9870ca5914a33d72fb8af98277a1992
daviddevops96 <david.marchan.devops@gmail.com>
2022-12-20 12:29:58 +0100

    #1#Init

---
+++ src/main/java/org/example/Account.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Account {
+    private String accountNumber;
+    private String pin;
+    private double balance;
+    // Other properties for an Account...
+
+    public Account(String accountNumber, String pin, double balance) {
+        this.accountNumber = accountNumber;
+        this.pin = pin;
+        this.balance = balance;
+    }
+
+    // Getters and setters for Account properties...
+}
+
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Person {
+    private String name;
+    private String address;
+    // Other properties for a Person...
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    // Getters and setters for Person properties...
+}
